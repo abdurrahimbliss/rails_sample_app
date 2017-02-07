23de0ad5aeca01dd70ae1020bfc04aa6ceda2ab0
abdurrahim.ceg@gmail.com <rahim@Mohameds-MacBook-Pro.local>
2017-02-07 11:57:58 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Rahim"
+    email "rahim@figs.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,7 +23,7 @@ describe User do
   describe "when email format is invalid" do
     it "should not be valid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                      foo@bar_baz.com foo@bar+baz.com]
+                      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -78,6 +78,16 @@ describe User do
       let(:invalid_password_result) { found_user.authenticate("invalidpassword") }
       it { should_not eq invalid_password_result }
       specify { expect(invalid_password_result).to be_false }
+    end
+  end
+
+  describe "email with mixed cases" do
+    let(:mixed_case_email) { 'fOOl@Example.com' }
+
+    it "should be saved as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
 end
